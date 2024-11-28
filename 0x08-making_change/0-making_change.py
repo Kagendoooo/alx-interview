0732bdc6f55709453644c885710f3fd52a30aa1b
Gendozzzz <sunflowermaina@gmail.com>
2024-11-29 01:29:20 +0300

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,7 +8,6 @@ def makeChange(coins, total):
     """Fewest number of coins needed to meet a given amount total"""
     if total <= 0:
         return 0
-    coins.sort(reverse=True)
     dloop = [float('inf')] * (total + 1)
     dloop[0] = 0
     for coin in coins:
