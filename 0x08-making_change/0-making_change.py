223a8615acfb1e039c8dc3fc706f70e183751111
Gendozzzz <sunflowermaina@gmail.com>
2024-11-29 01:14:22 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Make change from coins
+"""
+
+
+def makeChange(coins, total):
+    """Fewest number of coins needed to meet a given amount total"""
+    if total <= 0:
+        return 0
+    dloop = [float('inf')] * (total + 1)
+    dloop[0] = 0
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dloop[i] = min(dloop[i], dloop[i - coin] + 1)
+    return dloop[total] if dloop[total] != float('inf') else -1
